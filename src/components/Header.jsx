8c6befe680962307b5405b837e20200934aa47de
MaxChuloshnikov <Maks447@ukr.net>
2023-02-01 15:37:30 +0200

    Dashboard complete
    + Team, Contacts and Invoices pages done

---
+++ src/components/Header.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { tokens } from "../theme";
+import { Box, Typography, useTheme } from "@mui/material";
+
+
+const Header = ({title, subtitle}) => {
+    const theme = useTheme();
+    const colors = tokens(theme.palette.mode);
+
+
+    return (
+        <Box>
+            <Typography variant="h1" fontSize="32px" fontWeight="600">
+                {title}
+            </Typography>
+            <Typography variant="p" color={colors.greenAccent[500]}>
+                {subtitle}
+                </Typography>
+        </Box>
+    )
+}
+
+export default Header;
--- src/scenes/global/Sidebar.jsx
+++ src/scenes/global/Sidebar.jsx
@@ -111,10 +111,10 @@ const Item = ({ title, to, icon, selected, setSelected }) => {
                     fontWeight="bold"
                     sx={{ m: "10px 0 0 0" }}
                   >
-                    Ed Roh
+                    Max Ch
                   </Typography>
                   <Typography variant="h5" color={colors.greenAccent[500]}>
-                    VP Fancy Admin
+                    Admin
                   </Typography>
                 </Box>
               </Box>
