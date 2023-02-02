13a9f8832bf5b23229e7536b6525c0350c378877
MaxChuloshnikov <Maks447@ukr.net>
2023-02-02 13:04:07 +0200

    FAQ Done
    Accordion from MUI

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import Contacts from "./scenes/contacts";
 import Form from "./scenes/form";
 //import Line from "./scenes/line";
 //import Pie from "./scenes/pie";
-//import FAQ from "./scenes/faq";
+import FAQ from "./scenes/faq";
 //import Geography from "./scenes/geography";
 import Calendar from "./scenes/calendar";
 
@@ -33,13 +33,14 @@ const App = () => {
               <Routes>
                 <Route path="/" element={<Dashboard/>}/>
                 <Route path="/team" element={<Team/>}/> 
-               <Route path="/invoices" element={<Invoices/>}/> 
-               <Route path="/contacts" element={<Contacts/>}/>
-              {/* <Route path="/bar" element={<Bar/>}/> */}
-               <Route path="/form" element={<Form/>}/>
+                <Route path="/invoices" element={<Invoices/>}/> 
+                <Route path="/contacts" element={<Contacts/>}/>
+                <Route path="/form" element={<Form/>}/>
+                <Route path="/faq" element={<FAQ/>}/>
               {/* <Route path="/line" element={<Line/>}/> */}
+              {/* <Route path="/bar" element={<Bar/>}/> */}
               {/*  <Route path="/pie" element={<Pie/>}/> */}
-              {/*  <Route path="/faq" element={<FAQ/>}/> */}
+             
               {/*  <Route path="/geography" element={<Geography/>}/> */}
               <Route path="/calendar" element={<Calendar/>}/>
               </Routes>
